d2becc226ac4e73198434eecfc6f5a6af7e1c371
matteo-luraghi <matteoluraghi50@gmail.com>
2024-11-08 18:49:42 +0100

    feat(logos and date): added some logos and date of activity of the project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,27 @@
 import "./css/Project.css";
 
 export default function Project({ project }) {
+  function formateDate(date) {
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const showDate = `${month}/${date.getFullYear()}`;
+    return showDate;
+  }
+
+  const startDate = new Date(project.start_date);
+  const endDate = new Date(project.end_date);
+
+  const showStartDate = formateDate(startDate);
+  const showEndDate = formateDate(endDate);
+
   return (
     <a href={project.url} className="project-card">
       <div className="project-card-header">
         <img src={project.image} className="project-logo" alt={project.title} />
         <h3>{project.title}</h3>
       </div>
+      <p>
+        {showStartDate}-{showEndDate}
+      </p>
       <p>{project.description}</p>
     </a>
   );
