e548d70a232d66d6888bf6dd01d29dc97cd8c1fe
matteo-luraghi <matteoluraghi50@gmail.com>
2024-11-09 11:48:33 +0100

    feat(fadein): cards fade in on scroll, and date is positioned below title

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,40 @@
+import { useEffect, useRef } from "react";
 import "./css/Project.css";
 
 export default function Project({ project }) {
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    // observer that observes when an element enters or leaves the viewport
+    const observer = new IntersectionObserver(
+      (entries) => {
+        // function that runs when the visibility of an element in the viewport changes
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.5 },
+    );
+
+    // attach the project to the observer (each project has a different ref since it's a different component)
+    if (cardRef.current) {
+      observer.observe(cardRef.current);
+    }
+
+    // save the observed reference for cleanup
+    const cardRefSaved = cardRef.current;
+
+    // cleanup function
+    return () => {
+      if (cardRefSaved) {
+        observer.unobserve(cardRefSaved);
+      }
+    };
+  }, []);
+
   function formateDate(date) {
     const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const showDate = `${month}/${date.getFullYear()}`;
@@ -14,15 +48,17 @@ export default function Project({ project }) {
   const showEndDate = formateDate(endDate);
 
   return (
-    <a href={project.url} className="project-card">
+    <a href={project.url} className="project-card" ref={cardRef}>
       <div className="project-card-header">
         <img src={project.image} className="project-logo" alt={project.title} />
-        <h3>{project.title}</h3>
+        <div className="project-title-container">
+          <h3 className="project-title">{project.title}</h3>
+          <p className="project-date">
+            {showStartDate}-{showEndDate}
+          </p>
+        </div>
       </div>
-      <p>
-        {showStartDate}-{showEndDate}
-      </p>
-      <p>{project.description}</p>
+      <p className="project-description">{project.description}</p>
     </a>
   );
 }
